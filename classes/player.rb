809b1df2e0a34e4092066afc81fe67fd6bdae776
Chris Kamp <chriskamp101@gmail.com>
2021-04-03 15:13:00 +1000

    add player move method

--- classes/player.rb
+++ classes/player.rb
@@ -1,7 +1,25 @@
+
+# Represents the player's character
 class Player
   attr_reader :coords
 
   def initialize(coords = { x: 2, y: 2 })
     @coords = coords
+
+    # Index of coord changes when moving in each direction
+    @move_index = {
+      left: { x: -1, y: 0 },
+      right: { x: 1, y: 0 },
+      up: { x: 0, y: -1 },
+      down: { x: 0, y: 1 }
+    }
+  end
+
+  # Given a direction to move, return the destination coords
+  def move(direction)
+    return {
+      x: @coords[:x] + @move_index[direction][:x],
+      y: @coords[:y] + @move_index[direction][:y]
+    }
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -7,8 +7,28 @@ describe Player do
   it "instantiates an object" do
     expect(@player).to_not be_nil
   end
-  it "has default coords of 2, 2" do
-    expect(@player.coords[:x]).to eq(2)
-    expect(@player.coords[:y]).to eq(2)
+
+  describe ".coords" do
+    it "has default value of 2, 2" do
+      expect(@player.coords[:x]).to eq(2)
+      expect(@player.coords[:y]).to eq(2)
+    end
+  end
+
+  describe ".move" do
+    it "returns the square to the left" do
+      expect(@player.move(:left)).to eq({x: 1, y: 2})
+    end
+    it "returns the square to the right" do
+      expect(@player.move(:right)).to eq({x: 3, y: 2})
+    end
+    it "returns the square above" do
+      expect(@player.move(:up)).to eq({x: 2, y: 1})
+    end
+    it "returns the square below" do
+      expect(@player.move(:down)).to eq({x: 2, y: 3})
+    end
+    xit "works from different starting coords" do
+    end
   end
 end
